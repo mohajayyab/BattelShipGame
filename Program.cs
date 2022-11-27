c4760d506a0591eaf0aad1bac70839a8d9eda0d1
MOHAMMED ABUJAYYAB <115325019+mohajayyab@users.noreply.github.com>
2022-11-27 12:33:43 +0100

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿using Battelship;
+using System;
+
+namespace Battelship_new
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Board first = new Board();
+            first.loadfile("board1.txt");
+            first.loadfile("board2.txt");
+
+            //first.Initialization(10);
+            first.placeships1(4);
+            first.ship1array();
+            first.placeships2();
+            first.ship2array();
+            first.Visualize();
+            first.RunGame();
+        }
+    }
+}
